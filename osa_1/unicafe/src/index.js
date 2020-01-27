fc0f6f5d4e161022d25b53549459c0108285ebc1
Ville Marttila <ville.marttila@iki.fi>
2020-01-27 21:23:34 +0200

    tehtävä 1.8 tehty

--- osa_1/unicafe/src/index.js
+++ osa_1/unicafe/src/index.js
@@ -9,13 +9,27 @@ const Button = (props) => {
     )
 }
 
+const Statistics = ({ good, neutral, bad }) => {
+    const all = good + neutral + bad
+    const average = (good + (bad * -1)) / all
+    const positive = good / all * 100
+
+    return (
+        <p>
+            good {good}<br />
+            neutral {neutral}<br />
+            bad {bad}<br />
+            all {all}<br />
+            average {all === 0 ? 0 : average}<br />
+            positive {all === 0 ? 0 : positive} %
+        </p>
+    )
+}
+
 const App = () => {
     const [ good, setGood ] = React.useState(0)
     const [ neutral, setNeutral ] = React.useState(0)
     const [ bad, setBad ] = React.useState(0)
-    const all = good + neutral + bad
-    const average = (good + (bad * -1)) / all
-    const positive = good / all * 100
     
     const clickGood = () => setGood(good + 1)
     const clickNeutral = () => setNeutral(neutral + 1)
@@ -27,15 +41,8 @@ const App = () => {
             <Button handleClick={clickGood} text='good' />
             <Button handleClick={clickNeutral} text='neutral' />
             <Button handleClick={clickBad} text='bad' />
-            <h1>statistics</h1>
-            <p>
-                good {good}<br />
-                neutral {neutral}<br />
-                bad {bad}<br />
-                all {all}<br />
-                average {all === 0 ? 0 : average}<br />
-                positive {all === 0 ? 0 : positive} %
-            </p>
+            <h1>statistics</h1>    
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 
