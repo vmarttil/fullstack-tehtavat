04878feff6f0b67898fb1121be50bfd69f15c13c
Ville Marttila <ville.marttila@iki.fi>
2020-01-27 22:31:51 +0200

    tehtävä 1.13 tehty

--- osa_1/anekdootit/src/index.js
+++ osa_1/anekdootit/src/index.js
@@ -10,15 +10,32 @@ const Button = (props) => {
   )
 }
 
+const Votes = (props) => {
+  return (
+    <>
+      has {props.votes[props.selected]} votes<br /
+    ></>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   const clickRandom = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const clickVote = () => {
+    const newTable = { ...votes }
+    newTable[selected] += 1
+    setVotes(newTable)
+  }
 
   return (
     <div>
       {props.anecdotes[selected]}<br />
+      <Votes selected={selected} votes={votes} />
+      <Button text='vote' handleClick={clickVote} />
       <Button text='next anecdote' handleClick={clickRandom} />
+
     </div>
   )
 }
