9a5e4e18412533f7bc31054d76d582633762c544
Ville Marttila <ville.marttila@iki.fi>
2020-02-01 18:48:53 +0200

    tehtävä 2.7 tehty

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -15,7 +15,12 @@ const App = () => {
     const personObject = {
       name: newName
     }
-    setPersons(persons.concat(personObject))
+    if (persons.some(p => p.name === newName)) {
+      
+      window.alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+    }
     setNewName('')
   }
 
